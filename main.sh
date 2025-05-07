e4523abc3cc006ddc692ff16be5fabc676a53d7b
drong <rongx038@gmail.com>
2025-05-06 20:56:33 -0400

    add spot

--- main.sh
+++ main.sh
@@ -16,12 +16,29 @@ printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
 python bhds.py aws_kline download-um-futures "1m"
 python bhds.py aws_kline verify-type-all um_futures "1m"
 python bhds.py aws_kline parse-type-all um_futures "1m"
-# python bhds.py api_data download-aws-missing-kline-type um_futures "1m"
+python bhds.py api_data download-aws-missing-kline-type um_futures "1m"
 printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
 python bhds.py generate kline-type um_futures "1m" --split-gaps --with-vwap --with-funding-rates
 python bhds.py generate resample-type um_futures "1h" "0m"
-printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
 # ================================================
 # download cm data
 # ================================================
+# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
 # python bhds.py aws_funding download-cm-futures
+# python bhds.py aws_funding verify-type-all cm_futures
+# python bhds.py aws_funding parse-type-all cm_futures
+# python bhds.py api_data download-recent-funding-type cm_futures
+# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+
+# ================================================
+# download spot data
+# ================================================
+printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+python bhds.py aws_kline download-spot "1m"
+python bhds.py aws_kline verify-type-all spot "1m"
+python bhds.py aws_kline parse-type-all spot "1m"
+python bhds.py api_data download-aws-missing-kline-type spot "1m"
+printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+python bhds.py generate kline-type spot "1m" --split-gaps --with-vwap --no-with-funding-rates
+python bhds.py generate resample-type spot "1h" "0m"
+printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
