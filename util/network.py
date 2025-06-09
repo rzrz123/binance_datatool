aecee8b7cb560455e70a778808ab0e9fcb952116
drong <rongx038@gmail.com>
2025-06-08 20:25:14 -0400

    sync bmac

--- util/network.py
+++ util/network.py
@@ -5,7 +5,7 @@ import json
 
 from .log_kit import logger
 
-BINANCE_CODES = {-1122}
+BINANCE_CODES = {-1122, -1121}
 
 class BinanceAPIException(Exception):
 
@@ -34,7 +34,6 @@ async def async_retry_getter(func, _max_times=5, _sleep_seconds=1, **kwargs):
         except BinanceAPIException as e:
             if e.code in BINANCE_CODES:
                 logger.warning(e)
-                logger.debug(e.response)
                 return None
 
             raise e
