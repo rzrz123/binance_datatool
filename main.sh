86d6b960ef41e0c4c8d2a625375568a4c8dc0d0a
drong <rongx038@gmail.com>
2025-10-17 13:03:28 -0400

    finial my datawarehouse

--- main.sh
+++ main.sh
@@ -5,20 +5,19 @@ hline() { printf '=%.0s' $(seq 1 ${1:-100}); }
 # ================================================
 # download um data
 # ================================================
-# 用函数动态生成指定长度的分隔线（默认30个=）
-printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
-python datalake.py aws_funding download-um-futures
-python datalake.py aws_funding verify-type-all um_futures
-python datalake.py aws_funding parse-type-all um_futures
-python datalake.py api_data download-recent-funding-type um_futures
-printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
-python datalake.py aws_kline download-um-futures "1m"
-python datalake.py aws_kline verify-type-all um_futures "1m"
-python datalake.py aws_kline parse-type-all um_futures "1m"
-# python datalake.py api_data download-aws-missing-kline-type um_futures "1m"
-printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
-python datalake.py generate kline-type binance um_futures "1m" --split-gaps --with-vwap --with-funding-rates
-python datalake.py generate resample-type binance um_futures "1h"
+# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+# python datalake.py aws_funding download-um-futures
+# python datalake.py aws_funding verify-type-all um_futures
+# python datalake.py aws_funding parse-type-all um_futures
+# python datalake.py api_data download-recent-funding-type um_futures
+# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+# python datalake.py aws_kline download-um-futures "1m"
+# python datalake.py aws_kline verify-type-all um_futures "1m"
+# python datalake.py aws_kline parse-type-all um_futures "1m"
+# # python datalake.py api_data download-aws-missing-kline-type um_futures "1m"
+# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+# python datalake.py generate kline-type binance um_futures "1m" --split-gaps --with-vwap --with-funding-rates
+# python datalake.py generate resample-type binance um_futures "1h"
 # ================================================
 # download cm data
 # ================================================
@@ -39,15 +38,14 @@ python datalake.py generate resample-type binance um_futures "1h"
 # ================================================
 # download spot data
 # ================================================
-# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
-# python datalake.py aws_kline download-spot "1m"
-# python datalake.py aws_kline verify-type-all spot "1m"
-# python datalake.py aws_kline parse-type-all spot "1m"
+printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+python datalake.py aws_kline download-spot "1m"
+python datalake.py aws_kline verify-type-all spot "1m"
+python datalake.py aws_kline parse-type-all spot "1m"
 # python datalake.py api_data download-aws-missing-kline-type spot "1m"
-# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
-# python datalake.py generate kline-type spot "1m" --split-gaps --with-vwap --no-with-funding-rates
-# python datalake.py generate resample-type spot "1h" "0m"
-# printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+printf '\e[32m%s\e[0m | %s |\n' "$(date +%T)" "$(hline)"
+python datalake.py generate kline-type binance spot "1m" --split-gaps --with-vwap --no-with-funding-rates
+python datalake.py generate resample-type binance spot "1h"
 
 # ================================================
 # download bybit data
